31d1ac5267ad8b16acb6ececd2f2cc6e285c2d40
Heaven <1302888286@qq.com>
2017-12-13 11:13:37 +0800

    脚手架搭建

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+//设置为 false 以阻止 vue 在启动时生成生产提示。
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
