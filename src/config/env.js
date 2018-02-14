078fb1920f96fd4e1ea2aac6b3e857ba9284ce74
Heaven <1302888286@qq.com>
2018-02-14 20:58:33 +0800

    Update

--- src/config/env.js
+++ src/config/env.js
@@ -2,7 +2,7 @@
 // let routerMode = 'history';     
 let routerMode = 'hash';
 let baseUrl = "/api";
-// let baseUrl = "http://169.254.25.131:3007";
+// let baseUrl = "http://39.108.168.223:3000";
 
 export {
   routerMode,
